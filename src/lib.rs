7812854d1eaa32e70f6a36dedb7298ac979b4e84
Sam <sam@teuida.net>
2025-10-22 19:32:08 +0900

    add subscription post endpoint and write tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,17 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -10,6 +20,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| { 
         App::new()
            .route("/health_check", web::get().to(health_check))
+           .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
